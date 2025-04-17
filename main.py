c445f03d9cb4389e7c696882cf58a3ab9e25d404
dan_bar <150222525+Daniils12334@users.noreply.github.com>
2025-04-17 09:37:40 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+class Student:
+    def __init__(self, id, name, encoding, registered=True):
+        self.id = id
+        self.name = name
+        self.encoding = encoding
+        self.registered = registered
