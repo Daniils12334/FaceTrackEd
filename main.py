800417804a0882b5db5372d682e8eb3f65b19ea6
dan_bar <150222525+Daniils12334@users.noreply.github.com>
2025-04-17 11:09:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 class Student:
-    def __init__(self, id, name, encoding, registered=True):
+    def __init__(self, id, name, encoding, registered=True, age, form):
         self.id = id
         self.name = name
         self.encoding = encoding
         self.registered = registered
+        self.age = age
+        self.form = form
